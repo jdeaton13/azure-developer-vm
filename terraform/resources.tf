5b038f3377c896abadee9a1ccd8731c4839fd27c
Joshua Deaton <joshua.deaton@fisglobal.com>
2021-10-28 11:02:04 -0400

    Fix validation errors

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -28,28 +28,28 @@ resource "azurerm_resource_group" "rg_development_sandbox" {
 }
 
 resource "azurerm_virtual_network" "vnet_development_sandbox" {
-  resource_group_name = azurerm_resource_group.rg_development_sandbox
+  resource_group_name = azurerm_resource_group.rg_development_sandbox.name
   name                = "vnet-development-sandbox"
   location            = azurerm_resource_group.rg_development_sandbox.location
   address_space       = var.cidr_block
 }
 
 resource "azurerm_subnet" "snet_development_sandbox_public" {
-  resource_group_name = azurerm_resource_group.rg_development_sandbox
-  name                = "snet-development-sandbox-public"
-  location            = azurerm_resource_group.rg_development_sandbox.location
-  address_prefixes    = var.public_subnet
+  resource_group_name  = azurerm_resource_group.rg_development_sandbox.name
+  name                 = "snet-development-sandbox-public"
+  virtual_network_name = azurerm_virtual_network.vnet_development_sandbox.name
+  address_prefixes     = var.public_subnet
 }
 
 resource "azurerm_public_ip" "pip_development_sandbox" {
-  resource_group_name = azurerm_resource_group.rg_development_sandbox
+  resource_group_name = azurerm_resource_group.rg_development_sandbox.name
   name                = "pip-development-sandbox"
   location            = azurerm_resource_group.rg_development_sandbox.location
   allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "nic_development_sandbox_public" {
-  resource_group_name = azurerm_resource_group.rg_development_sandbox
+  resource_group_name = azurerm_resource_group.rg_development_sandbox.name
   name                = "nic-development-sandbox-public"
   location            = azurerm_resource_group.rg_development_sandbox.location
 
@@ -62,7 +62,7 @@ resource "azurerm_network_interface" "nic_development_sandbox_public" {
 }
 
 resource "azurerm_network_interface" "nic_development_sandbox_internal" {
-  resource_group_name = azurerm_resource_group.rg_development_sandbox
+  resource_group_name = azurerm_resource_group.rg_development_sandbox.name
   name                = "nic-development-sandbox-internal"
   location            = azurerm_resource_group.rg_development_sandbox.location
 
@@ -74,7 +74,7 @@ resource "azurerm_network_interface" "nic_development_sandbox_internal" {
 }
 
 resource "azurerm_network_security_group" "nsg-development-sandbox" {
-  resource_group_name = azurerm_resource_group.rg_development_sandbox
+  resource_group_name = azurerm_resource_group.rg_development_sandbox.name
   name                = "nic-development-sandbox-internal"
   location            = azurerm_resource_group.rg_development_sandbox.location
 
@@ -106,7 +106,7 @@ resource "azurerm_network_interface_security_group_association" "assoc-nic-nsg" 
 }
 
 resource "azurerm_windows_virtual_machine" "vm_development_sandbox" {
-  resource_group_name = azurerm_resource_group.rg_development_sandbox
+  resource_group_name = azurerm_resource_group.rg_development_sandbox.name
   name                = "vm-development-sandbox"
   location            = azurerm_resource_group.rg_development_sandbox.location
   size                = "Standard_D2ds_v4"
@@ -122,5 +122,10 @@ resource "azurerm_windows_virtual_machine" "vm_development_sandbox" {
     offer     = "windows-11"
     sku       = "win11-21h2-entn"
     version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "StandardSSD_LRS"
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,7 @@ variable "region" {
 }
 
 variable "cidr_block" {
-  default = "10.0.0.0/16"
+  default = ["10.0.0.0/16"]
 }
 
 variable "public_subnet" {
